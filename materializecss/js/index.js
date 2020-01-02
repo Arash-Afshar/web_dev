a3286f3cc6d3787bf4ebc6cf73b7306f9ea45ae8
Arash Afshar <arash.afshar.edu@gmail.com>
2020-01-02 13:09:56 -0700

    added minimal materializecss

---
+++ materializecss/js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+$(document).ready(function(){
+  $('.scrollspy').scrollSpy();
+});
+
+let addedTransparent = false
+let addedShadow = false
+
+function scrollFunction() {
+  if (document.body.scrollTop > 64 || document.documentElement.scrollTop > 64) {
+    document.getElementsByTagName("nav")[0].classList.remove("transparent")
+    document.getElementsByTagName("nav")[0].style.backgroundColor = "rgba(255, 255, 255, 0.9)"
+    document.getElementsByTagName("nav")[0].classList.remove("z-depth-0")
+    if (!addedShadow) {
+      document.getElementsByTagName("nav")[0].classList.add("z-depth-1")
+      addedShadow = true
+    }
+    addedTransparent = false
+  } else {
+    if (!addedTransparent) {
+      document.getElementsByTagName("nav")[0].classList.add("transparent")
+      document.getElementsByTagName("nav")[0].classList.add("z-depth-0")
+      document.getElementsByTagName("nav")[0].classList.remove("z-depth-1")
+      addedTransparent = true
+      addedShadow = false
+    }
+  }
+}
+
+window.onscroll = function() {scrollFunction()};
